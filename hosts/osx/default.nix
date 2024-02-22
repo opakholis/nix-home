5b461affe5a0e768c3d37ce20288e6087697b93f
Opa Kholis Majid <code@opakholis.space>
2024-02-23 06:22:53 +0700

    feat(core): storage optimization

--- hosts/osx/default.nix
+++ hosts/osx/default.nix
@@ -7,8 +7,25 @@
   # Make sure the nix daemon always runs,
   services.nix-daemon.enable = true;
 
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
+  nix = {
+    # https://nixos.wiki/wiki/Storage_optimization
+    gc = {
+      automatic = true;
+      interval = {
+        Weekday = 0;
+        Hour = 0;
+        Minute = 0;
+      };
+      options = "--delete-older-than 30d";
+    };
+
+    # `experimental-features` | enable nix flakes
+    # `auto-optimise-store` | optimise nix store every build
+    settings = {
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;
+    };
+  };
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
