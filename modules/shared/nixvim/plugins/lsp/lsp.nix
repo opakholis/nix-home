e82c1f1541202591feab4d897e8d31d3a58430ec
Opa Kholis Majid <code@opakholis.space>
2024-09-29 21:37:49 +0700

    refactor(nixvim/lsp): update diagnostics info sign

--- modules/shared/nixvim/plugins/lsp/lsp.nix
+++ modules/shared/nixvim/plugins/lsp/lsp.nix
@@ -91,10 +91,13 @@
       };
     };
     extraConfigLuaPre = ''
-      vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticError", linehl = "", numhl = "" })
-      vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticWarn", linehl = "", numhl = "" })
-      vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticHint", linehl = "", numhl = "" })
-      vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
+      do
+        local diagnostic_signs = { Error = "", Warn = "", Hint = "󰌵", Info = "" }
+        for type, icon in pairs(diagnostic_signs) do
+          local hl = "DiagnosticSign" .. type
+          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        end
+      end
     '';
     extraConfigLua = ''
       -- Change border of documentation hover window
