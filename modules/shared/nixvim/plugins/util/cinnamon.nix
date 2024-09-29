172b2e4b7fe008ae976a6f818ccf3f81909d24f1
Opa Kholis Majid <code@opakholis.space>
2024-09-29 16:44:54 +0700

    feat(nixvim): add cinnamon

---
+++ modules/shared/nixvim/plugins/util/cinnamon.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+{
+  programs.nixvim = {
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "cinnamon";
+        src = pkgs.fetchFromGitHub {
+          owner = "declancm";
+          repo = "cinnamon.nvim";
+          rev = "450cb3247765fed7871b41ef4ce5fa492d834215";
+          hash = "sha256-kccQ4iFMSQ8kvE7hYz90hBrsDLo7VohFj/6lEZZiAO8=";
+        };
+      })
+    ];
+    extraConfigLua = ''
+      require("cinnamon").setup {
+          keymaps = {
+              basic = true,
+              extra = true,
+          },
+          max_delta = {
+            time = 100,
+          },
+      }
+    '';
+  };
+}
