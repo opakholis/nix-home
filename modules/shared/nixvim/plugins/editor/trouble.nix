6c3051f3b9a6dce336b6b122fc1cde593ed602b3
Opa Kholis Majid <code@opakholis.space>
2024-09-29 16:33:05 +0700

    feat(nixvim/keymap): add trouble diagnostics

--- modules/shared/nixvim/plugins/editor/trouble.nix
+++ modules/shared/nixvim/plugins/editor/trouble.nix
@@ -1,11 +1,32 @@
 { ... }:
 {
-  programs.nixvim.plugins = {
-    trouble = {
-      enable = true;
-      settings = {
-        auto_close = true;
+  programs.nixvim = {
+    plugins = {
+      trouble = {
+        enable = true;
+        settings = {
+          auto_close = true;
+        };
       };
+
+      which-key.settings.spec = [
+        {
+          __unkeyed = "<leader>x";
+          group = "Trouble";
+          icon = " ";
+        }
+      ];
     };
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>xx";
+        action = "<cmd>Trouble diagnostics<cr>";
+        options = {
+          desc = "Diagnostics";
+        };
+      }
+    ];
   };
 }
