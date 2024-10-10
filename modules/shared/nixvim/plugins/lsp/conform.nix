05252448a43add1be7f41f5e76910da151d98565
Opa Kholis Majid <code@opakholis.space>
2024-10-10 20:19:38 +0700

    fix(nixvim/lsp): prioritize prettierd

--- modules/shared/nixvim/plugins/lsp/conform.nix
+++ modules/shared/nixvim/plugins/lsp/conform.nix
@@ -16,14 +16,15 @@ in
         formatters_by_ft =
           let
             prettier = {
-              __unkeyed-1 = "eslint_d";
-              __unkeyed-2 = "prettierd";
+              __unkeyed-1 = "prettierd";
+              __unkeyed-2 = "prettier";
               stop_after_first = true;
             };
           in
           {
             lua = [ "stylua" ];
             nix = [ "nixfmt" ];
+            json = prettier;
             javascript = prettier;
             javascriptreact = prettier;
             typescript = prettier;
