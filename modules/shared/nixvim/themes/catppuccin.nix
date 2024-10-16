489e64fb666333edfe00c539c2507dc53e8d9eb1
Opa Kholis Majid <code@opakholis.space>
2024-10-16 22:16:14 +0700

    refactor(nixvim/theme): override cattpuccin's treesitter token colors"

--- modules/shared/nixvim/themes/catppuccin.nix
+++ modules/shared/nixvim/themes/catppuccin.nix
@@ -27,11 +27,46 @@
         custom_highlights = ''
           function(cp)
             return {
-            -- base config
-            NormalFloat = { bg = cp.none },
-            Pmenu = { bg = cp.none },
-            PmenuBar = { bg = cp.surface0 },
-            TabLineSel = { bg = cp.pink },
+              -- base config
+              NormalFloat = { bg = cp.none },
+              Pmenu = { bg = cp.none },
+              PmenuBar = { bg = cp.surface0 },
+              TabLineSel = { bg = cp.pink },
+
+              -- plugin
+              TroubleNormal = { bg = cp.none },
+
+              -- treesitter
+              ["@field"] = { fg = cp.rosewater },
+              ["@property"] = { fg = cp.yellow },
+
+              ["@include"] = { fg = cp.teal },
+              ["@operator"] = { fg = cp.sky },
+              ["@keyword.operator"] = { fg = cp.sky },
+              ["@punctuation.special"] = { fg = cp.maroon },
+
+              ["@constructor"] = { fg = cp.lavender },
+              ["@exception"] = { fg = cp.peach },
+
+              ["@constant.builtin"] = { fg = cp.lavender },
+              ["@variable.builtin"] = { fg = cp.red, style = { "italic" } },
+
+              ["@function.macro"] = { fg = cp.red, style = {} },
+              ["@parameter"] = { fg = cp.rosewater },
+              ["@keyword.function"] = { fg = cp.maroon },
+              ["@keyword"] = { fg = cp.red },
+              ["@keyword.return"] = { fg = cp.pink, style = {} },
+
+              ["@method"] = { style = { "italic" } },
+              ["@namespace"] = { fg = cp.rosewater, style = {} },
+
+              ["@punctuation.delimiter"] = { fg = cp.teal },
+              ["@punctuation.bracket"] = { fg = cp.overlay2 },
+              ["@variable"] = { fg = cp.text },
+              ["@tag.attribute"] = { fg = cp.mauve, style = { "italic" } },
+              ["@tag"] = { fg = cp.peach },
+              ["@tag.delimiter"] = { fg = cp.maroon },
+              ["@text"] = { fg = cp.text },
             }
           end
         '';
