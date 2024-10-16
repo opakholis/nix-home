222303426e6a7f475a48b23bc2f2ca67fdf2f11b
Opa Kholis Majid <code@opakholis.space>
2024-10-16 22:10:13 +0700

    style(nixvim): move colorschemes to separate folder

--- modules/shared/nixvim/default.nix
+++ modules/shared/nixvim/default.nix
@@ -3,6 +3,7 @@
   imports = [
     ./options
     ./plugins
+    ./themes
   ];
 
   programs.nixvim = {
--- modules/shared/nixvim/options/default.nix
+++ modules/shared/nixvim/options/default.nix
@@ -4,6 +4,5 @@
     ./autocmd.nix
     ./settings.nix
     ./keymaps.nix
-    ./themes.nix
   ];
 }
