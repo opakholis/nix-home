40255bf15d1a29c1f7b56f85eae3932aa157d2e5
Opa Kholis Majid <code@opakholis.space>
2024-10-16 20:54:50 +0700

    feat(nixvim/bufferline): custom close buffer

--- modules/shared/nixvim/plugins/ui/bufferline.nix
+++ modules/shared/nixvim/plugins/ui/bufferline.nix
@@ -9,6 +9,11 @@
           diagnostics = "nvim_lsp";
           mode = "buffers";
 
+          close_command.__raw = ''
+            function(bufnr)
+              buf_kill("bd", bufnr, false)
+            end
+          '';
           close_icon = " ";
           buffer_close_icon = "󰱝 ";
           modified_icon = "󰔯 ";
@@ -27,8 +32,8 @@
     keymaps = [
       {
         mode = "n";
-        key = "<leader>bd";
-        action = "<cmd>bdelete<cr>";
+        key = "<leader>bb";
+        action = "<cmd>lua buf_kill()<cr>";
         options = {
           desc = "Delete buffer";
         };
@@ -57,5 +62,82 @@
         action = "<c-6>"; # switch to last buffer
       }
     ];
+
+    extraConfigLua = ''
+      -- Common kill function for bdelete and bwipeout
+      -- Credits: Based on bbye and nvim-bufdel from LunarVim
+      -- Source: https://github.com/LunarVim/LunarVim/blob/85ccca97acfea9a465e354e18bb2f6109ba417f8/lua/lvim/core/bufferline.lua#L184
+      function buf_kill(kill_command, bufnr, force)
+        kill_command = kill_command or "bd"
+
+        local bo = vim.bo
+        local api = vim.api
+        local fmt = string.format
+        local fn = vim.fn
+
+        if bufnr == 0 or bufnr == nil then
+          bufnr = api.nvim_get_current_buf()
+        end
+
+        local bufname = api.nvim_buf_get_name(bufnr)
+
+        if not force then
+          local choice
+          if bo[bufnr].modified then
+            choice = fn.confirm(fmt([[Save changes to "%s"?]], bufname), "&Yes\n&No\n&Cancel")
+            if choice == 1 then
+              vim.api.nvim_buf_call(bufnr, function()
+                vim.cmd("w")
+              end)
+            elseif choice == 2 then
+              force = true
+            else return
+            end
+          elseif api.nvim_get_option_value("buftype", { buf = 0 }) == "terminal" then
+            choice = fn.confirm(fmt([[Close "%s"?]], bufname), "&Yes\n&No\n&Cancel")
+            if choice == 1 then
+              force = true
+            else
+              return
+            end
+          end
+        end
+
+        -- Get list of windows IDs with the buffer to close
+        local windows = vim.tbl_filter(function(win)
+          return api.nvim_win_get_buf(win) == bufnr
+        end, api.nvim_list_wins())
+
+        if force then
+          kill_command = kill_command .. "!"
+        end
+
+        -- Get list of active buffers
+        local buffers = vim.tbl_filter(function(buf)
+          return api.nvim_buf_is_valid(buf) and bo[buf].buflisted
+        end, api.nvim_list_bufs())
+
+        -- If there is only one buffer (which has to be the current one), vim will
+        -- create a new buffer on :bd.
+        -- For more than one buffer, pick the previous buffer (wrapping around if necessary)
+        if #buffers > 1 and #windows > 0 then
+          for i, v in ipairs(buffers) do
+            if v == bufnr then
+              local prev_buf_idx = i == 1 and #buffers or (i - 1)
+              local prev_buffer = buffers[prev_buf_idx]
+              for _, win in ipairs(windows) do
+                api.nvim_win_set_buf(win, prev_buffer)
+              end
+            end
+          end
+        end
+
+        -- Check if buffer still exists, to ensure the target buffer wasn't killed
+        -- due to options like bufhidden=wipe.
+        if api.nvim_buf_is_valid(bufnr) and bo[bufnr].buflisted then
+          vim.cmd(string.format("%s %d", kill_command, bufnr))
+        end
+      end
+    '';
   };
 }
