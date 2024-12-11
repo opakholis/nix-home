5b6c5efd7afc488fceef9c02040c5e032c7dbcde
Opa Kholis Majid <code@opakholis.space>
2024-12-12 01:25:59 +0700

    refactor(nixvim/lsp): remove unused lint autocmd

--- modules/shared/nixvim/plugins/lsp/lint.nix
+++ modules/shared/nixvim/plugins/lsp/lint.nix
@@ -40,18 +40,7 @@
     ];
 
     extraConfigLua = ''
-      -- Init nvim-eslint
       require('nvim-eslint').setup({})
-
-      -- Autocmd lint
-      local lint = require("lint")
-      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        group = lint_augroup,
-        callback = function()
-          lint.try_lint()
-        end,
-      })
     '';
   };
 }
