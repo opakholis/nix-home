a0528b7a2fcdcf8ee7968bd2bde8f3fcfb6a4e57
Opa Kholis Majid <code@opakholis.space>
2025-01-26 13:21:29 +0700

    refactor(nixvim/copilot): upstream changes

--- modules/shared/nixvim/plugins/util/copilot.nix
+++ modules/shared/nixvim/plugins/util/copilot.nix
@@ -6,20 +6,22 @@
     };
     plugins.copilot-lua = {
       enable = true;
-      filetypes = {
-        "*" = true;
-      };
-      panel = {
-        enabled = false;
-      };
-      suggestion = {
-        enabled = false;
-        autoTrigger = true;
-        keymap = {
-          accept = "<C-a>";
-          dismiss = "<C-d>";
-          next = "<C-]>";
-          prev = "<C-[>";
+      settings = {
+        filetypes = {
+          "*" = true;
+        };
+        panel = {
+          enabled = false;
+        };
+        suggestion = {
+          enabled = false;
+          autoTrigger = true;
+          keymap = {
+            accept = "<C-a>";
+            dismiss = "<C-d>";
+            next = "<C-]>";
+            prev = "<C-[>";
+          };
         };
       };
     };
