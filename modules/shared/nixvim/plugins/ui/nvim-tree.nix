114698680e5c684664d32d8121278f2906dc2785
Opa Kholis Majid <code@opakholis.space>
2025-08-19 07:56:41 +0700

    refactor(nixvim): breaking change nvimtree config

--- modules/shared/nixvim/plugins/ui/nvim-tree.nix
+++ modules/shared/nixvim/plugins/ui/nvim-tree.nix
@@ -4,57 +4,55 @@
     plugins.nvim-tree = {
       enable = true;
       autoClose = true;
-      hijackCursor = true;
-      modified.enable = true;
-      filters = {
-        custom = [
-          "node_modules"
-          "^\.git"
-          "^\.cache"
-        ];
-      };
-      renderer = {
-        highlightGit = true;
-        rootFolderLabel = false;
-        indentMarkers = {
-          enable = true;
+      settings = {
+        hijack_cursor = true;
+        modified.enable = true;
+        update_focused_file.enable = true;
+        filters = {
+          custom = [
+            "node_modules"
+            "^\.git"
+            "^\.cache"
+          ];
         };
-        icons = {
-          show.git = false;
+        renderer = {
+          indent_markers.enable = true;
+          highlight_git = true;
+          root_folder_label = false;
+          icons = {
+            show.git = false;
+          };
         };
-      };
-      updateFocusedFile = {
-        enable = true;
-      };
-      onAttach.__raw = ''
-        function (bufnr)
-          local api = require "nvim-tree.api"
-          local function opts(desc)
-            return {
-              desc = "nvim-tree: " .. desc,
-              buffer = bufnr,
-              noremap = true,
-              silent = true,
-              nowait = true
+        on_attach.__raw = ''
+          function (bufnr)
+            local api = require "nvim-tree.api"
+            local function opts(desc)
+              return {
+                desc = "nvim-tree: " .. desc,
+                buffer = bufnr,
+                noremap = true,
+                silent = true,
+                nowait = true
+              }
+            end
+
+            api.config.mappings.default_on_attach(bufnr)
+
+            local mappings = {
+              ["l"] = { api.node.open.edit, opts "Open" },
+              ["o"] = { api.node.open.edit, opts "Open" },
+              ["<CR>"] = { api.node.open.edit, opts "Open" },
+              ["v"] = { api.node.open.vertical, opts "Open: Vertical Split" },
+              ["h"] = { api.node.navigate.parent_close, opts "Close Directory" },
+              ["C"] = { api.tree.change_root_to_node, opts "CD" },
             }
+
+            for k, v in pairs(mappings) do
+              vim.keymap.set('n', k, v[1], v[2])
+            end
           end
-
-          api.config.mappings.default_on_attach(bufnr)
-
-          local mappings = {
-            ["l"] = { api.node.open.edit, opts "Open" },
-            ["o"] = { api.node.open.edit, opts "Open" },
-            ["<CR>"] = { api.node.open.edit, opts "Open" },
-            ["v"] = { api.node.open.vertical, opts "Open: Vertical Split" },
-            ["h"] = { api.node.navigate.parent_close, opts "Close Directory" },
-            ["C"] = { api.tree.change_root_to_node, opts "CD" },
-          }
-
-          for k, v in pairs(mappings) do
-            vim.keymap.set('n', k, v[1], v[2])
-          end
-        end
-      '';
+        '';
+      };
     };
 
     keymaps = [
